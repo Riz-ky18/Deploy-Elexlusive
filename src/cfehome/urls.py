2a7e9d711f3fb1ec11782e4aeaf236632479e6d4
Mokyra18 <rizkyramadhani181102@gmail.com>
2024-07-15 20:55:28 +0700

    first commit

---
+++ src/cfehome/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import home_view, about_view
+
+urlpatterns = [
+    path("", home_view), #index page -> root page
+    path("about/", about_view),
+    path("hello-world/", home_view),
+    path("hello-world.html", home_view),
+    path("admin/", admin.site.urls),
+]
